eba46a64e12dd7db90028cafa293f49c19059243
moerwald <andreas.mewald@gmail.com>
2021-08-14 22:04:59 +0200

    Added ItemsControl project

---
+++ src/ItemControls/ItemsControl/ItemsControl/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItemsControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly List<Person> _persons = new List<Person>
+        {
+            new Person { Name = "Steve", Age = 34},
+            new Person { Name = "Peggy", Age = 30},
+            new Person { Name = "Dan", Age = 25}
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            itemsControl.ItemsSource = _persons;
+        }
+    }
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
